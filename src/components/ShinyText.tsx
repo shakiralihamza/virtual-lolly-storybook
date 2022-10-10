514361f84e79cfe8d4afcf6c59d3fa2bd8fa5892
Shakir Ali <shakiralihamza05@gmail.com>
2022-10-11 04:32:56 +0500

    added new font

--- src/components/ShinyText.tsx
+++ src/components/ShinyText.tsx
@@ -6,19 +6,20 @@ interface ShinyTextProps {
     size: string;
     italic: boolean;
 }
-const ShinyText:FC<ShinyTextProps> = ({text, size, italic}) => (
+
+const ShinyText: FC<ShinyTextProps> = ({text, size, italic}) => (
     <Typography
         sx={{
             '&:hover': {
-                textShadow:'#fa73d9 0 0 16px'
+                textShadow: '#fa73d9 0 0 16px'
             },
             fontSize: size,
-            fontWeight:300,
+            fontWeight: 300,
             fontStyle: italic ? 'italic' : 'normal',
-            color:'#fff',
-            borderBottomStyle:'none',
-            textShadow:'#fa73d9 0 0 8px',
-            fontFamily:'"Yellowtail", cursive',
+            color: '#fff',
+            borderBottomStyle: 'none',
+            textShadow: '#fa73d9 0 0 8px',
+            fontFamily: 'Gochi Hand',
         }}
     >
         {text}
